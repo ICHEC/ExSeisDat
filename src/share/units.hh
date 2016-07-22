2a82cf201aca8f4dd74098358e5e56e5d6341359
Cathal O Broin <cathal.obroin4@mail.dcu.ie>
2016-07-22 11:18:51 +0100

    Add file for units

---
+++ src/share/units.hh
@@ -0,0 +1,23 @@
+/*******************************************************************************************//*!
+ *   \file
+ *   \author Cathal O Broin - cathal@ichec.ie - first commit
+ *   \copyright TBD. Do not distribute
+ *   \date July 2016
+ *   \brief This file contains any necessary SI units, conversion factors, physical constants
+ *   etc
+ *   \details
+ *//*******************************************************************************************/
+#ifndef PIOLSHAREUNITS_INCLUDE_GUARD
+#define PIOLSHAREUNITS_INCLUDE_GUARD
+#include "global.hh"
+namespace PIOL {
+namespace SI                        //!< This namespace is being used like an enum
+{
+    const unit_t Micro = 1e-6L;    //!< Micro in SI units
+}
+namespace Physical                  //!< For physical constants
+{
+
+}
+}
+#endif
