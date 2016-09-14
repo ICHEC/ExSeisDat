7d4686e89050511c69dbfd184e11bf0bc1a8b9fb
Cathal O Broin <cathal@ichec.ie>
2016-09-14 09:52:51 +0100

    Add test based on random inputs

--- test/opstest.cc
+++ test/opstest.cc
@@ -57,3 +57,42 @@ TEST_F(OpsTest, getMinMaxFail)  //These fails won't surive a multi-processor exa
     piol->isErr();
 }
 
+
+template <bool Y, bool Min>
+void testMinMax(const std::vector<coord_t> & coord, const std::vector<CoordElem> & minmax)
+{
+    double val = double(Min ? 1 : -1) * std::numeric_limits<double>::infinity();
+    size_t tn = 0;
+    for (size_t i = 0; i < coord.size(); i++)
+    {
+        double t = (Y ? coord[i].y : coord[i].x);
+        if ((t < val && Min) || (t > val && !Min))
+        {
+            val = t;
+            tn = i;
+        }
+    }
+    ASSERT_DOUBLE_EQ(val, minmax[2U*size_t(Y) + size_t(!Min)].val);
+    ASSERT_EQ(tn, minmax[2U*size_t(Y) + size_t(!Min)].num);
+}
+
+TEST_F(OpsTest, getMinMaxRand)
+{
+    srand(31337);
+    std::vector<CoordElem> minmax(4);
+    for (size_t l = 0; l < 20; l++)
+    {
+        size_t num = rand() % 1710880;
+        std::vector<coord_t> coord(num);
+        for (size_t i = 0; i < num; i++)
+            coord[i] = { rand(), rand() };
+
+        getMinMax(piol, 0, coord.size(), coord.data(), minmax.data());
+        piol->isErr();
+        testMinMax<false, false>(coord, minmax);
+        testMinMax<false, true>(coord, minmax);
+        testMinMax<true, false>(coord, minmax);
+        testMinMax<true, true>(coord, minmax);
+    }
+}
+
