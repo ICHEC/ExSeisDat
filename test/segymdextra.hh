9a3a95c2f2d79b6e6151b6b3739be22458065418
Cathal O Broin <cathal@ichec.ie>
2016-10-27 13:33:54 +0100

    For test purposes

---
+++ test/segymdextra.hh
@@ -0,0 +1,211 @@
+/*******************************************************************************************//*!
+ *   \file
+ *   \author Cathal O Broin - cathal@ichec.ie - first commit
+ *   \copyright TBD. Do not distribute
+ *   \date October 2016
+ *   \brief
+ *   \details
+ *//*******************************************************************************************/
+#ifndef PIOLFILESEGYMD_INCLUDE_GUARD
+#define PIOLFILESEGYMD_INCLUDE_GUARD
+
+#include <cstring>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <limits>
+#include "global.hh"
+#include "file/filesegy.hh"
+#include "object/object.hh"
+#include "share/segy.hh"
+#include "file/iconv.hh"
+#include "share/units.hh"
+#include "share/datatype.hh"
+
+namespace PIOL { namespace File {
+/*! The Datatype (or format in SEG-Y terminology)
+ */
+enum class Format : int16_t
+{
+    IBM  = 1,   //<! IBM format, big endian
+    TC4  = 2,   //<! Two's complement, 4 byte
+    TC2  = 3,   //<! Two's complement, 2 byte
+    FPG  = 4,   //<! Fixed-point gain (obsolete)
+    IEEE = 5,   //<! The IEEE format, big endian
+    NA1  = 6,   //<! Unused
+    NA2  = 7,   //<! Unused
+    TC1  = 8    //<! Two's complement, 1 byte
+};
+
+/*! Header offsets as defined in the specification. Actual offset is the value minus one.
+ */
+enum class Hdr : size_t
+{
+    Increment  = 3217U, //!< int16_t. The increment between traces in microseconds
+    NumSample  = 3221U, //!< int16_t. The number of samples per trace
+    Type       = 3225U, //!< int16_t. Trace data type. AKA format in SEGY terminology
+    Sort       = 3229U, //!< int16_t. The sort order of the traces.
+    Units      = 3255U, //!< int16_t. The unit system, i.e SI or imperial.
+    SEGYFormat = 3501U, //!< int16_t. The SEG-Y Revision number
+    FixedTrace = 3503U, //!< int16_t. Whether we are using fixed traces or not.
+    Extensions = 3505U, //!< int16_t. If we use header extensions or not.
+};
+/*! Misc Trace Header offsets
+ */
+enum class TrHdr : size_t
+{
+    SeqNum      = 1U,   //!< int32_t. The trace sequence number within the Line
+    SeqFNum     = 5U,   //!< int32_t. The trace sequence number within SEG-Y File
+    ORF         = 9U,   //!< int32_t. The original field record number.
+    TORF        = 13U   //!< int32_t. The trace number within the ORF.
+};
+
+/*! Trace Header offsets to elevations
+ */
+enum class TrElev : size_t
+{
+    RcvElv      = 41U,  //!< int32_t. The Receiver group elevation
+    SurfElvSrc  = 45U,  //!< int32_t. The surface elevation at the source.
+    SrcDpthSurf = 49U,  //!< int32_t. The source depth below surface (opposite of above?).
+    DtmElvRcv   = 53U,  //!< int32_t. The datum elevation for the receiver group.
+    DtmElvSrc   = 57U,  //!< int32_t. The datum elevation for the source.
+    WtrDepSrc   = 61U,  //!< int32_t. The water depth for the source.
+    WtrDepRcv   = 65U   //!< int32_t. The water depth for the receive group.
+};
+
+/*! Trace Header offsets to scalers
+ */
+enum class TrScal : size_t
+{
+    ScaleElev   = 69U,  //!< int16_t. The scale coordinate for 41-68 (elevations + depths)
+    ScaleCoord  = 71U   //!< int16_t. The scale coordinate for 73-88 + 181-188
+};
+
+/*! Trace Header offsets to coordinates
+ */
+enum class TrCrd : size_t
+{
+    xSrc = 73U,  //!< int32_t. The X coordinate for the source
+    ySrc = 77U,  //!< int32_t. The Y coordinate for the source
+    xRcv = 81U,  //!< int32_t. The X coordinate for the receive group
+    yRcv = 85U,  //!< int32_t. The Y coordinate for the receive group
+    xCMP = 181U, //!< int32_t  The X coordinate for the CMP
+    yCMP = 185U  //!< int32_t. The Y coordinate for the CMP
+};
+
+/*! Trace Header offsets to grid components
+ */
+enum class TrGrd : size_t
+{
+    il        = 189U, //!< int32_t. The Inline grid point.
+    xl        = 193U  //!< int32_t. The Crossline grid point.
+};
+
+/*! \brief Return a pair of coordinate offsets
+ *  \param[in] pair The coordinate point type to return
+ *  \return The pair of offset enums
+ */
+#ifndef __ICC
+constexpr
+#else
+inline
+#endif
+std::pair<TrCrd, TrCrd> getPair(Coord pair)
+{
+    switch (pair)
+    {
+        case Coord::Src :
+            return std::make_pair(TrCrd::xSrc, TrCrd::ySrc);
+        case Coord::Rcv :
+            return std::make_pair(TrCrd::xRcv, TrCrd::yRcv);
+        default :
+        case Coord::CMP :
+            return std::make_pair(TrCrd::xCMP, TrCrd::yCMP);
+    }
+}
+
+/*! \brief Return a pair of grid offsets
+ *  \param[in] pair The grid point type to return
+ *  \return The pair of offset enums
+ */
+#ifndef __ICC
+constexpr
+#else
+inline
+#endif
+std::pair<TrGrd, TrGrd> getPair(Grid pair)
+{
+    switch (pair)
+    {
+//Note: When a new set of grid points are required:
+//        case Grid::OFR :
+//            return std::make_pair(TrHdr::ORF, TrHdr::TORF);
+        default :
+        case Grid::Line :
+            return std::make_pair(TrGrd::il, TrGrd::xl);
+    }
+}
+
+extern geom_t scaleConv(int16_t scale);
+extern geom_t getMd(const TrScal scal, const uchar * src);
+extern geom_t getMd(const TrCrd item, const geom_t scale, const uchar * src);
+extern int32_t getMd(const TrGrd item, const uchar * src);
+extern void setCoord(const Coord item, const coord_t coord, const int16_t scale, uchar * buf);
+extern coord_t getCoord(const Coord item, const geom_t scale, const uchar * buf);
+extern grid_t getGrid(const Grid item, const uchar * buf);
+extern void setGrid(const Grid item, const grid_t grid, uchar * buf);
+extern int16_t deScale(const geom_t val);
+extern int16_t scalComp(int16_t scal1, int16_t scal2);
+extern int16_t calcScale(const coord_t coord);
+extern void extractTraceParam(const uchar * md, TraceParam * prm);
+extern void insertTraceParam(const TraceParam * prm, uchar * md);
+extern void setScale(const TrScal item, const int16_t scale, uchar * buf, size_t start = 0);
+
+/*! \brief Get the header metadata value from the binary header.
+ *  \param[in] item The header item of interest.
+ *  \param[in] src The buffer of the header object.
+ *  \return Return the header item value.
+ */
+template <class T = int16_t>
+T getMd(const Hdr item, const uchar * src)
+{
+    switch (item)
+    {
+        case Hdr::Increment :
+        case Hdr::Type :
+        case Hdr::NumSample :
+        case Hdr::Units :
+        case Hdr::SEGYFormat :
+        case Hdr::FixedTrace :
+        case Hdr::Extensions :
+        return T(getHost<int16_t>(&src[size_t(item)-1U]));
+        default :
+        return T(0);
+        break;
+    }
+}
+
+/*! \brief Set the header metadata value corresponding to the item specified
+ *  \param[in] item The header item of interest
+ *  \param[in] src The metadata value to insert into the buffer.
+ *  \param[in, out] dst The header as an array of uchar.
+ */
+template <typename T = int16_t>
+void setMd(const Hdr item, const T src, uchar * dst)
+{
+    switch (item)
+    {
+        case Hdr::Increment :
+        case Hdr::Type :
+        case Hdr::NumSample :
+        case Hdr::Units :
+        case Hdr::SEGYFormat :
+        case Hdr::FixedTrace :
+        case Hdr::Extensions :
+        getBigEndian<int16_t>(src, &dst[static_cast<size_t>(item)-1U]);
+        default :
+        break;
+    }
+}
+}}
+#endif
