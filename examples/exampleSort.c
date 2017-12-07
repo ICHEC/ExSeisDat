ec141f4e2e24af3bd8a4b540466931d63367a14c
Meghan Fisher <meghanfisher@fionn3.cb3.ichec.ie>
2017-12-07 16:52:31 +0000

    examples C/C++ for sorting, bandpass/taper

---
+++ examples/exampleSort.c
@@ -0,0 +1,12 @@
+#include "flow.h"
+int main(void)
+{
+    PIOL_ExSeis* piol = PIOL_ExSeis_new(PIOL_VERBOSITY_NONE);
+    PIOL_Set* set = PIOL_Set_new(piol, "/ichec/work/exseisdat/dat/10*/b*");
+    PIOL_Set_output(set, "presort");
+    PIOL_Set_sort(set, PIOL_SORTTYPE_OffLine);
+    PIOL_Set_output(set, "postsort");
+    PIOL_Set_delete(set);
+    PIOL_ExSeis_delete(piol);
+    return 0;
+}
---
+++ examples/exampleSort.cc
@@ -0,0 +1,16 @@
+/*******************************************************************************************//*!
+ *   \file
+ *   \author Cathal O Broin - cathal@ichec.ie - first commit
+ *   \copyright TBD. Do not distribute
+ *   \date February 2017
+ *   \brief Sort a file in 9 lines
+*//*******************************************************************************************/
+#include "flow.hh"
+using namespace PIOL;
+int main(void)
+{
+    ExSeis piol;
+    Set set(piol, "/ichec/work/exseisdat/dat/10*/b*", "temp");
+    set.sort(PIOL_SORTTYPE_OffLine);
+    return 0;
+}
