0c6c153bd4a3b9f7f644bbe1a92d64a1748857ad
Padraig O Conbhui <padraig.oconbhui@ichec.ie>
2018-01-08 13:48:39 +0000

    Added missing wraptests.

---
+++ test/wraptests/setwraptests.hh
@@ -0,0 +1,9 @@
+#ifndef PIOLWRAPTESTSSETWRAPTESTS_INCLUDE_GUARD
+#define PIOLWRAPTESTSSETWRAPTESTS_INCLUDE_GUARD
+
+#include <memory>
+#include "mockexseis.hh"
+
+void test_PIOL_Set(std::shared_ptr<PIOL::ExSeis*> piol);
+
+#endif // PIOLWRAPTESTSSETWRAPTESTS_INCLUDE_GUARD
---
+++ test/wraptests/writedirectwraptests.hh
@@ -0,0 +1,13 @@
+#ifndef PIOLWRAPTESTSWRITEDIRECTWRAPTESTS_INCLUDE_GUARD
+#define PIOLWRAPTESTSWRITEDIRECTWRAPTESTS_INCLUDE_GUARD
+
+#include<memory>
+#include "mockexseis.hh"
+#include "mockparam.hh"
+
+void test_PIOL_File_WriteDirect(
+    std::shared_ptr<PIOL::ExSeis*> piol,
+    std::shared_ptr<PIOL::File::Param*> param
+);
+
+#endif // PIOLWRAPTESTSWRITEDIRECTWRAPTESTS_INCLUDE_GUARD
