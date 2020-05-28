ae0d17513f64706bfa3191863165cb2fc2e3a2cb
Pádraig Ó Conbhuí <padraig.oconbhui@ichec.ie>
2020-05-28 23:15:04 +0100

    Add Catch2 v2.12.1 to third_party

--- third_party/third_party.cmake
+++ third_party/third_party.cmake
@@ -13,9 +13,25 @@ include(${CMAKE_CURRENT_LIST_DIR}/find_fftw3.cmake)
 #
 # Find CLI11
 #
-add_library(exseisdat_CLI11 INTERFACE)
-target_include_directories(
-    exseisdat_CLI11
-    SYSTEM INTERFACE ${CMAKE_CURRENT_LIST_DIR}/CLI11-v1.7.1
+add_library(exseisdat::CLI11 INTERFACE IMPORTED)
+set_property(
+    TARGET exseisdat::CLI11
+    PROPERTY
+        INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/CLI11-v1.7.1
 )
-add_library(exseisdat::CLI11 ALIAS exseisdat_CLI11)
+set_property(
+    TARGET exseisdat::CLI11
+    PROPERTY
+        INTERFACE_COMPILE_DEFINITIONS CLI11_EXPERIMENTAL_OPTIONAL=0
+)
+
+
+#
+# Find Catch2
+#
+add_library(exseisdat::catch2 INTERFACE IMPORTED)
+set_property(
+    TARGET exseisdat::catch2
+    PROPERTY
+        INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/catch-v2.12.1
+)
